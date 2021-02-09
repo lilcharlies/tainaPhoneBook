756650c213ee443205fed0048a95293e17b34698
lilcharlies <72659963+lilcharlies@users.noreply.github.com>
2021-02-09 14:36:14 +0200

    Add files via upload

--- fullstack project/JavaScript/index.js
+++ fullstack project/JavaScript/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 
-
 //getting the input boxs
 const Name = document.getElementById("name");
 const Number = document.getElementById("number");
@@ -12,53 +11,72 @@ const submit = document.querySelector('.item1');
 const contactList = document.querySelector('.items_list');
 
 
-
-
-
 function template(data){
-    contactList.insertAdjacentHTML("afterbegin",`
-    <div class="listWrapper" id="${data.name.value}" onClick="charlie(this.id)">
+  var className = data.name.replace(" ", "");
+  return `
+    <section class="${className}">
+    <div class="listWrapper" id="${className}"  onclick="addDesc('${className}')">
     <div class="listinfo">
-     <span>${data.name.value}</span>
-     <span>${data.number.value}</span>
+     <span>${data.name}</span>
+     <span>${data.number}</span>
     </div>
      <div class="imgcont">
-        <div class="cont"><img src="../imges/trash.png" alt="trash"></div>
+        <div class="cont" onclick='deleteContact("${className}")'><img src="../imges/trash.png" alt="trash"></div>
         <div class="cont"><img src="../imges/edit.png" alt="edit"></div>
     </div>
     </div>
-    `)
+    </section>
+    `;
+
 }
+
+//get data from local storage to inject it into html
+for (let i = 0; i < localStorage.length;i++){
+  const saved = localStorage.getItem(localStorage.key(i));
+  contactList.innerHTML += template(JSON.parse(saved));
+}
+
+function deletecontact(ke){
+  document.getElementsByClassName(ke)[0].innerHTML = '';
+  localStorage.removeItem(ke);
+}
+
+
+
 
 //event listener too the send button to get the data the user entered and put it in the localstorae
 submit.addEventListener('click',addContact,false);
 
+let contactArray = Array();
+
 
 function addContact(e){
-  if(keysInStorage(Name.value))
-    return;
+  if(keysInStorage(Name.value)) return;
     const data ={
-        name : Name,
-        number : Number,
-        address : Address,
-        email : Email,
-        desc : Desc
+        name : Name.value,
+        number : Number.value,
+        address : Address.value,
+        email : Email.value,
+        desc : Desc.value
     }
+    contactArray.push(data);
     
     e.preventDefault();
-    if(data.name.value == "" || data.number.value == "") return;
+    if(data.name == "" || data.number == "") return;
     
-    template(data);
-    
-    localStorage.setItem(Name.value ,contactList.innerHTML);
+    //var htmlString = template(data);
+    var keyName = data.name.replace(" ","");
+    localStorage.setItem(keyName,JSON.stringify(data));
+    contactList.innerHTML +=template(data);
+
+
     Name.value = "";
     Number.value = "";
     Address.value = "";
     Email.value = "";
     Desc.value = "";
-    
-  }    
-  
+  }
+
   //delte all ocntact function
   function delAllContact(e){
     contactList.remove();
@@ -89,107 +107,66 @@ function keysInStorage(value){
 
 
 
+//!!!!!!!!!!!!!!!!!       descreption section             !!!!!!!!!!!!!
+
+const myModal2 = document.getElementById("myModal2");
+const close2 = document.getElementById("close2");
+
+if(document.querySelectorAll(".listWrapper")){
+  console.log(document.querySelectorAll(".listWrapper"));
+//document.querySelectorAll(".listWrapper")[0].style.display = "block";
+}
 
 
-const DescContainer = document.querySelector(".descContainer");
+close2.onclick = function(){
+  myModal2.style.display = "none";
+}
 
-// function addDesc(e){
-//     const data ={
-//         name : Name,
-//         number : Number,
-//         address : Address,
-//         email : Email,
-//         desc : Desc
-//     }    
-//     console.log(data);
+
+//const descContainer = document.getElementsByClassName("descContainer")[0];
+
+function addDesc(name){
+
+    // for(let i=0; i < listWrapper.length;i++){
+    //   listWrapper[i].style.display = "block";
+    //  }
+
+  var details = localStorage.getItem(localStorage.key(name));
+  details = JSON.parse(details);
+
   
-//     e.preventDefault();
-    
-//     tampletDesc(data);
+  descContainer.innerHTML = desctamplate(details);
+  console.log(descContainer);
 
-// }    
-
+}
 
 
-
-
-// if(document.querySelector(".descContainer")){
-// btn2.addEventListener('click',addDesc);  
-// }
-
-
-
-
-
-
-//get data from local storage to inject it into html
-for (var key in localStorage){
-  const saved = localStorage.getItem(localStorage.key(key));
-      contactList.innerHTML = saved;
+function desctamplate(details){
+  return `
+    <section>
+    <div class="listWrapper">
+    <div class="listinfo">
+     <span>${details.name}</span>
+     <span>${details.number}</span>
+    </div>
+    </div>
+    </section>
+    `;
 }
 
 
 
 
 
-
-
-
-
-
-//search localstorage function
-const srcbtn = document.querySelector('.srcimg');
-srcbtn.addEventListener('click',search);
-
-function search(e){
-  e.preventDefault();
-  const value = document.querySelector('.srcbar').value;
-  var search = localStorage.getItem('contactListing');
-  console.log(search);
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//modal buttons selectors
-
-
-
+//!!!!!!!!!!!!!!!!!    Add contact modal overlay     !!!!!!!!!!!!!!!!!!
 
 const modal = document.getElementById("myModal");
-const myModal2 = document.getElementById("myModal2");
-
 
 // Get the button that opens the modal
 const btn = document.querySelector(".item1");
 
-
-
-
 // Get the <span> element that closes the modal
-const span = document.querySelector("span");
-const span2 = document.getElementById("close2");
-const listWrapper = document.querySelectorAll(".listWrapper");
-
-
+const close = document.querySelector(".close");
 
 
 // When the user clicks on the button, open the modal
@@ -198,32 +175,17 @@ add.onclick = function() {
 }
 
 
-if(document.querySelector(".listWrapper")){
-  for(let i=0; i < listWrapper.length;i++){
-    listWrapper[i].onclick = function(){
-    myModal2.style.display = "block";
-   }
- }
-}
-
-//     function charlie(id){
-//       for(let i=0; i < listWrapper.length;i++){
-//         listWrapper[i] =  myModal2.style.display = "block";
-//    }
-//  }
-
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+close.onclick = function() {
   modal.style.display = "none";
 }
-span2.onclick = function(){
-  myModal2.style.display = "none";
-}
+
 
 btn.onclick = function() {
   modal.style.display = "none";
-  location.reload();
+  //location.reload();
 }
+
 
 
 
